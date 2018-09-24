637214775f84f451d6bb0f5905ef2682d8ad803b
Max <clairson.max@gmail.com>
2018-09-24 20:49:40 +0200

    First step

--- app/src/main/java/com/footaddict/superstars/footaddict/MainActivity.java
+++ app/src/main/java/com/footaddict/superstars/footaddict/MainActivity.java
@@ -1,12 +1,16 @@
 package com.footaddict.superstars.footaddict;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
-import org.json.JSONObject;
-
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -16,39 +20,73 @@ import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
-    private JSONObject test;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        run("http://livescore-api.com/api-client/scores/history.json?key=yEcqTDm6UkJ51IqJ&secret=zVPESkhMLdIJENucrGCljZrekbjmTK5t");
-    }
-
-    private void run(String url) {
         OkHttpClient client = new OkHttpClient();
+
+        // Request for get list of all league
         Request request = new Request.Builder()
-                .url(url)
-                .header("Authorization", "0859e454-1a16-41bb-bcab-9d034f0594c2")
+                .url("http://livescore-api.com/api-client/leagues/list.json?key=yEcqTDm6UkJ51IqJ&secret=zVPESkhMLdIJENucrGCljZrekbjmTK5t")
                 .build();
 
+        //Call http request
         client.newCall(request).enqueue(new Callback() {
-            @Override
             public void onFailure(Call call, IOException e) {
-                Log.i("Thread",Thread.currentThread().toString());
-                Log.e("Data", "Erreur lors de la récupération des données", e);
+                e.printStackTrace();
+                Log.e("Exception", "Http connection failed ");
             }
-
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                Log.i("Thread",Thread.currentThread().toString());
-                final String body = response.body().string();
-                Log.i("Data", body);
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                    }
-                });
+            public void onResponse(Call call, final Response response) throws IOException {
+                if (!response.isSuccessful()) {
+                    throw new IOException("Unexpected code " + response);
+                } else {
+                // do something wih the result
+                    writeToFile(response.body().string(),"Leagues.json");
+                    System.out.println(readFromFile("Leagues.json"));
+                }
             }
         });
+    }
+
+    private void writeToFile(String data, String fileName) {
+        try {
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(openFileOutput(fileName, Context.MODE_PRIVATE));
+            outputStreamWriter.write(data);
+            outputStreamWriter.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            Log.e("Exception", "File write failed: " + e.toString());
+        }
+    }
+
+    private String readFromFile(String fileName) {
+
+        String result = "";
+        String receiveString = "";
+
+        try {
+            InputStream inputStream = openFileInput(fileName);
+
+            if ( inputStream != null ) {
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                StringBuilder stringBuilder = new StringBuilder();
+
+                while ( (receiveString = bufferedReader.readLine()) != null ) {
+                    stringBuilder.append(receiveString);
+                }
+
+                inputStream.close();
+                result = stringBuilder.toString();
+            }
+        } catch (FileNotFoundException e) {
+            Log.e("login activity", "File not found: " + e.toString());
+        } catch (IOException e) {
+            Log.e("login activity", "Can not read file: " + e.toString());
+        }
+
+        return result;
     }
 }
