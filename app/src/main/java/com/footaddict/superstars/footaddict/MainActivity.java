e570464117de68b5d4fe053fe7385e765b60e48b
Unknown <francoisjeannin9@gmail.com>
2018-09-24 11:49:28 +0200

    add run and okhttp dependency

--- app/src/main/java/com/footaddict/superstars/footaddict/MainActivity.java
+++ app/src/main/java/com/footaddict/superstars/footaddict/MainActivity.java
@@ -2,6 +2,15 @@ package com.footaddict.superstars.footaddict;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +19,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+/*
+    private void run(String url) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(url)
+                .header("Authorization", "0859e454-1a16-41bb-bcab-9d034f0594c2")
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.i("Thread",Thread.currentThread().toString());
+                Log.e("Data", "Erreur lors de la récupération des données", e);
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                Log.i("Thread",Thread.currentThread().toString());
+                final String body = response.body().string();
+                Log.i("Data", body);
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        texte.setText(body);
+                    }
+                });
+            }
+        });
+    }*/
 }
